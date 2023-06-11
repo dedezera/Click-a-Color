b61edf33dd2928a11d29ff555591837ca32393c1
André Felipe <doro123andre@gmail.com>
2023-06-11 09:24:14 -0300

    responsivity updates

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const colors = ['Red', 'Grey', 'Green', 'Lightgreen', 'Springgreen', 'Red', 'Blue', 'Lightblue', 'Purple', 'salmon', 'Burlywood', 'Chocolate', 'Darkcyan', 'Cornflowerblue', 'Darksalmon', 'Black']
 
 const btn = document.getElementById('btn')
-
 const color = document.querySelector('.color')
 
-// No click
 
 btn.addEventListener('click', function(){
     const randomColor = getRandomColor()
-    document.body.style.background = colors[randomColor] // Aplicando cor ao background
- //   console.log(randomColor)
+    document.body.style.background = colors[randomColor]
     color.textContent = colors[randomColor]
-})
 
-// Gerar os núemros randômicos
+    if (randomColor === colors.indexOf("Black")) {
+        document.body.style.color = "White"
+    } else {
+        document.body.style.color = ""
+    }
+    
+})
 
 function getRandomColor(){
     return Math.floor(Math.random() * colors.length)
